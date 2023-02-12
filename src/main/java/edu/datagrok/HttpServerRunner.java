597a2a77de47ca8e15b5151ebb97caf07ceab2fa
Anastasiia Tkachuk <anastaisha.580@gmail.com>
2023-02-12 10:18:34 +0200

    add new ServerRunner

--- src/main/java/edu/datagrok/HttpServerRunner.java
+++
@@ -1,8 +0,0 @@
-package edu.datagrok;
-
-public class HttpServerRunner {
-    public static void main(String[] args) {
-        HttpServer httpServer = new HttpServer(8088, 10);
-        httpServer.run();
-    }
-}
